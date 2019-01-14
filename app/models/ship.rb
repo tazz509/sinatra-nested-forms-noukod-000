cf8ea5229a5a743444cc12a63df8f1a4541802c6
carly <carly@noukod.com>
2019-01-14 20:10:13 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_reader :name, :weight, :height
+
+SHIPS = []
+
+ def initialize(params)
+   @name = params[:name]
+   @weight = params[:weight]
+   @height = params[:height]
+   SHIPS << self
+ end
+
+ def self.all
+   SHIPS
+ end
+
 end
