6d4380c015ab2efc9b005d5681f7faf968410656
carly <carly@noukod.com>
2019-01-14 20:43:34 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+SHIPS = []
+
+ def initialize(params)
+   @name = params[:name]
+   @type = params[:type]
+   @booty = params[:booty]
+   SHIPS << self
+ end
+
+ def self.all
+   SHIPS
+ end
+
 end
