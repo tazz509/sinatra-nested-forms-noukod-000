5811a3c8a21a31b19c5f37c07424c5b523094326
carly <carly@noukod.com>
2019-01-14 20:14:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,17 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    post '/' do
+      erb :new
+    end
+
+    get '/' do
+    erb :show
+    end
 
   end
 end
