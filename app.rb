d8f34e20db92f2cf2b47770eaf7bbc5c5a817f65
carly <carly@noukod.com>
2019-01-14 20:31:52 +0000

    Automatically backed up by Learn

---
+++ app.rb
@@ -0,0 +1,28 @@
+require './environment'
+
+module FormsLab
+  class App < Sinatra::Base
+
+    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+        @pirate = Pirate.new params[:pirate]
+
+        params[:pirate][:ships].each do |ship|
+          Ship.new ship
+        end
+
+        @ship1 = Ship.all.first
+        @ship2 = Ship.all[1]
+
+        erb :'pirates/show'
+      end
+    end
+end
